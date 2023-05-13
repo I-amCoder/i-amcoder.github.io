941477c47fa1b84c993d1e22a7ee8262959e1846
Junaid Ali <Junaid Ali>
2023-05-13 08:33:13 +0500

    New Fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,31 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 const Hero = () => {
-  const [fadeIn, setFadeIn] = useState(false);
+  const [generatedText, setGeneratedText] = useState('');
+  const [showText, setShowText] = useState(false);
+  
+  useEffect(()=>{
+    const text = "I'm generating the text...";
 
-  useEffect(() => {
-    setFadeIn(true);
-  }, []);
+    let currentIndex=0;
+   
+    const generateText = () => {
+      if (currentIndex < text.length) {
+        setGeneratedText((prevText) => prevText + text[currentIndex]);
+        currentIndex++;
+        setTimeout(generateText, 100); // Delay between each character
+      } else {
+        setShowText(true);
+      }
+    };
+
+    generateText();
+    
+  },[])
 
   return (
     <div className="container main text-center pt-5">
-      <div className={`name p-2`}>Helo Junaid!</div>
+      <div  className={`generated-text name ${showText ? 'show' : ''}`}>{generatedText}</div>
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="card shadow">
