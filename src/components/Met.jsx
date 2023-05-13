163b9de8914dcbe1fc1edcd8e306ff860387b35a
Junaid Ali <Junaid Ali>
2023-05-13 15:44:13 +0500

    IMporvement

--- src/components/Met.jsx
+++ src/components/Met.jsx
@@ -13,6 +13,7 @@ const Met = () => {
                         <Typist cursor={{ element:'' }}>
                            <h3>
                             Do You Want to Connect on Whatsapp?
+                            <br />
                             <br /> If so click on below Button any time you want.
                             </h3> 
                             <Link href="whatsapp://send?phone=923041577841" className="btn-grad text-success">Junaid's Here</Link>
