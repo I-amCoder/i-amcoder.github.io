aacbe424c991330e25089088eff2b94acc5d188b
Junaid Ali <Junaid Ali>
2023-05-13 08:13:13 +0500

    Small

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
 
   return (
     <div className="container main text-center pt-5">
-      <div className={`name fade-in ${fadeIn?'fade-in-active':''}  p-2`}>Helo Junaid!</div>
+      <div className={`name p-2`}>Helo Junaid!</div>
       <div className="row justify-content-center">
         <div className="col-md-6">
           <div className="card shadow">
