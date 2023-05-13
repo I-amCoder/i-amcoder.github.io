a2b11711314044f7c1848306b5e02caecb5d4ec7
Junaid Ali <Junaid Ali>
2023-05-13 16:56:28 +0500

    Whatsapp Set

--- src/components/Met.jsx
+++ src/components/Met.jsx
@@ -16,7 +16,7 @@ const Met = () => {
                             <br />
                             <br /> If so click on below Button any time you want.
                             </h3> 
-                            <Link href="whatsapp://send?phone=923041577841" className="btn-grad text-success">Junaid's Here</Link>
+                            <Link href="https://we.me/923041577841" className="btn-grad text-success">Junaid's Here</Link>
                             
                         </Typist>
                     </div>
